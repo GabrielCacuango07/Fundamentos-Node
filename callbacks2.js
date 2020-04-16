2f9cfa443e0df74227977f2058d92262b6a50e27
Gabriel Cacuango <gabrielcacuango07@gmail.com>
2020-04-16 00:33:30 -0500

    Ingreso de promesas, callback2 y async await

---
+++ callbacks2.js
@@ -0,0 +1,65 @@
+/* 
+        callbacks 2 
+        Nombre: Gabriel Cacuango 
+*/
+// simulaciom de base 
+let empleados = [{
+        id: 1,
+        nombre: "Gabriel"
+    },
+    {
+        id: 2,
+        nombre: "Wilson"
+    },
+    {
+        id: 3,
+        nombre: "Ricardo"
+    }
+];
+
+let salarios = [{
+        id: 1,
+        salario: 800
+    },
+    {
+        id: 2,
+        salario: 950
+    }
+];
+// funcion flecha 
+let getEmpleado = (id, callback) => {
+    // find nos permite buscar en un objeto como un for 
+    let empleadoDB = empleados.find(empleado => empleado.id === id);
+    if (!empleadoDB) {
+        callback(`No existe un empleado con id ${id}`);
+    } else {
+        callback(null, empleadoDB);
+    }
+}
+
+let getSalario = (empleado, callback) => {
+    let salarioDB = salarios.find(salario => salario.id === empleado.id)
+
+    if (!salarioDB) {
+        callback(`No se encontró salario para el empleado ${empleado.nombre}`)
+    } else {
+        callback(null, { nombre: empleado.nombre, salario: salarioDB.salario })
+    }
+}
+
+
+getEmpleado(2, (err, empleado) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    getSalario(empleado, (err, respuesta) => {
+        // si existe un error lo captura y lo imprime
+        if (err) {
+            return console.log(err);
+        }
+        // si no envia una respuesta con el siguiente formato
+        console.log(`El salario de ${respuesta.nombre} es de ${respuesta.salario}`);
+    })
+
+});
---
+++ promesas.js
@@ -0,0 +1,80 @@
+/* 
+        Promesas 
+        Nombre: Gabriel Cacuango 
+*/
+// simulacion base de datos 
+let empleados = [{
+        id: 1,
+        nombre: "Santiago"
+    },
+    {
+        id: 2,
+        nombre: "Wendy"
+    },
+    {
+        id: 3,
+        nombre: "Leo"
+    }
+];
+
+let salarios = [{
+        id: 1,
+        salario: 800
+    },
+    {
+        id: 2,
+        salario: 950
+    }
+];
+
+let getEmpleado = (id) => {
+        // se crea una nueva promesa  siempre con  resolve y reject 
+        return new Promise((resolve, reject) => {
+            let empleadoDB = empleados.find(empleado => empleado.id === id);
+            if (!empleadoDB) {
+                // captura el error 
+                reject(`No existe un empleado con id ${id}`);
+            } else {
+                // devuelce el resu;tado de estar correcto 
+                resolve(empleadoDB);
+            }
+        });
+    }
+    // igual que el ejemplo anterior solo que al verificar compara con el empleado id que sean igual 
+let getSalario = (empleado) => {
+    return new Promise((resolve, reject) => {
+        let salarioDB = salarios.find(salario => salario.id === empleado.id)
+
+        if (!salarioDB) {
+            // mensaje de error
+            reject(`No se encontró salario para el empleado ${empleado.nombre}`);
+        } else {
+            // en caso que todo salga bien 
+            resolve({ nombre: empleado.nombre, salario: salarioDB.salario });
+        }
+
+    });
+}
+
+// ejemplo con varios errores 
+// getEmpleado(3).then(empleado => {
+//     getSalario(empleado).then(resp => {
+//         console.log(`El salario de ${resp.nombre} es de ${resp.salario}`);
+//     }, (err) => {
+//         console.log(err);
+//     });
+// }, (err) => {
+//     console.log(err);
+// });
+
+// Promesas en cadena
+
+// evita acumular los errores  y atrapa a todos en un cacht
+
+getEmpleado(2).then(empleado => {
+    return getSalario(empleado);
+}).then(resp => {
+    console.log(`El salario de ${resp.nombre} es de ${resp.salario}`);
+}).catch(err => {
+    console.log(err);
+});
